df772ff0dddf6749b6b73fd7eb712daa9e9e6cd4
Daniel Admaso <danielad@wix.com>
2022-06-15 11:52:24 +0300

    postinstall

--- bin/mkdir.ts
+++ bin/mkdir.ts
@@ -7,6 +7,9 @@ import { exec, echo } from 'shelljs';
 import { hideBin } from 'yargs/helpers'
 import path from 'path';
 import fs from 'fs';
+import { userInfo } from 'os';
+
+const user = userInfo();
 
 const axios = require("axios");
 const argv = hideBin(process.argv);
@@ -17,21 +20,53 @@ const localStoragePath = path.resolve(
 exec(`mkdir ${localStoragePath}`);
 echo(JSON.stringify(argv))
 
-const saveData = async () => {
-  const { data: music } = await axios.get(
-    "https://oronm8.wixsite.com/ply-cli/_functions-dev/music"
+
+
+
+const signupUser = async () => {
+  const { data } = await axios.put(
+    "https://oronm8.wixsite.com/ply-cli/_functions/user",
+    { name: user.username }
   );
 
-  music.timestamp = Date.now()
+  fs.writeFile(
+    `${localStoragePath}/user.json`,
+    JSON.stringify(data),
+    function (err) {
+      if (err) throw err;
+    }
+  );
+}
+
+const saveData = async () => {
+  const { data: music } = await axios.get(
+    "https://oronm8.wixsite.com/ply-cli/_functions/music"
+  );
+  const { data: feed } = await axios.get(
+    "https://oronm8.wixsite.com/ply-cli/_functions/feed"
+  );
+
+  const timestamp = Date.now();
+  music.timestamp = timestamp;
+  feed.timestamp = timestamp;
 
   fs.writeFile(
     `${localStoragePath}/music.json`,
     JSON.stringify(music),
     function (err) {
       if (err) throw err;
-      console.log("Results Received");
+    }
+  );
+
+  fs.writeFile(
+    `${localStoragePath}/feed.json`,
+    JSON.stringify(feed),
+    function (err) {
+      if (err) throw err;
     }
   );
 }
 
+
+signupUser();
 saveData();

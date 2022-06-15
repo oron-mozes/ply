478ba13a5ab41391b2ffa66accf81a7627755cea
mozesoron <oronm@wix.com>
2022-06-15 16:45:33 +0300

    play first element

--- src/Features/YT.ts
+++ src/Features/YT.ts
@@ -1,9 +1,16 @@
 import ytp from 'yt-play-cli';
-import { hideBin } from 'yargs/helpers'
+import { hideBin } from 'yargs/helpers';
+import path from 'path';
+import fs from 'fs';
 
-export default async function () {
+const localStoragePath = path.resolve(__dirname, '../../../../../.ply/local-storage')
+
+export default async function (duration:number = 1) {
   const argv = new Set(hideBin(process.argv));
+  const list:{playlist:{videoId:string}[]} = JSON.parse(await fs.readFileSync(`${localStoragePath}/music.json`, 'utf-8'));
+  // const relevantSongs = 
+  // console.log(list);
   if (argv.has('-p') || argv.has('-play') || argv.has('--play')) {
-    ytp.play("_grkKX2dKqc");
+    ytp.play(list.playlist[0].videoId);
   }
 }

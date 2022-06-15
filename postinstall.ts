85b3d317d1e7be6b2a79c2082216c8bd9e792c75
mozesoron <oronm@wix.com>
2022-06-15 19:34:59 +0300

    get user email

--- postinstall.ts
+++ postinstall.ts
@@ -8,24 +8,36 @@ import fs from 'fs';
 import { userInfo } from 'os';
 import { apiBaseUrl } from './consts';
 import { getLocalStorage } from './utils';
+import readline from 'readline';
+
 const user = userInfo();
 const axios = require("axios");
 
 exec(`mkdir -p ${getLocalStorage()}`);
 
 const signupUser = async () => {
-  const { data } = await axios.put(
-    `${apiBaseUrl}/user`,
-    { name: user.username }
-  );
-
-  fs.writeFile(
-    `${getLocalStorage()}/user.json`,
-    JSON.stringify(data),
-    function (err) {
-      if (err) throw err;
-    }
-  );
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.question("What is your email? ", async function(answer) {
+    console.log("Thank you for registering for our ply cli:", answer);
+    const { data } = await axios.put(
+      `${apiBaseUrl}/user`,
+      { name: user.username, email: answer.trim() }
+    );
+  
+    fs.writeFile(
+      `${getLocalStorage()}/user.json`,
+      JSON.stringify(data),
+      function (err) {
+        if (err) throw err;
+      }
+    );
+    rl.close();
+  });
+  
 }
 
 const saveData = async () => {

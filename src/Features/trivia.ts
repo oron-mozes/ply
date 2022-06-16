c8362fdeb24d47e1a44aecf2fb06d7ceb1ea7f35
Sagi Kakon <sagika@wix.com>
2022-06-16 12:28:03 +0300

    first commit

--- src/Features/trivia.ts
+++ src/Features/trivia.ts
@@ -2,15 +2,21 @@ import { hideBin } from 'yargs/helpers';
 import shell, { echo } from 'shelljs';
 import path from 'path';
 
+var isTerminalActive = false;
+
 export default async function () {
   const argv = new Set(hideBin(process.argv));
 
   if ( argv.has('--p-game')) {
     const pathToGame = path.resolve(__dirname, 'game.js');
     shell.exec(`open -a iTerm  ${pathToGame}`);
-    setTimeout(() => {
-      shell.echo('In game')
-      shell.exec('osascript -e \'tell application "iTerm" to tell current window to close current tab\' & exit');
-    }, 5000);
+    isTerminalActive = true;
+  }
+}
+
+export const closeTerminalIfNeeded = () => {
+  if (isTerminalActive) {
+    shell.exec('osascript -e \'tell application "iTerm" to tell current window to close current tab\' & exit');
+    isTerminalActive = false;
   }
 }

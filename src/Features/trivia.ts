3958ad48c20917fda2dc0c2bf2e6a8ae93971d6e
Sagi Kakon <sagika@wix.com>
2022-06-17 15:15:14 +0300

    new trivia

--- src/Features/trivia.ts
+++ src/Features/trivia.ts
@@ -8,15 +8,17 @@ export default async function () {
   const argv = new Set(hideBin(process.argv));
 
   if ( argv.has('--p-game')) {
-    const pathToGame = path.resolve(__dirname, 'game.js');
-    shell.exec(`open -a iTerm  ${pathToGame}`);
+    const pathToGame = path.resolve(__dirname, 'new-trivia.js');
+    shell.exec(`open -a Terminal ${pathToGame}`);
+    shell.exec('osascript -e \'tell application "System Events" to keystroke "f" using {control down, command down}\'');
     isTerminalActive = true;
   }
 }
 
 export const closeTerminalIfNeeded = () => {
   if (isTerminalActive) {
-    shell.exec('osascript -e \'tell application "iTerm" to tell current window to close current tab\' & exit');
+    shell.exec('osascript -e \'tell application "Terminal" to close first window\'');
+    // shell.exec('osascript -e \'tell application "iTerm" to tell current window to close current tab\' & exit');
     isTerminalActive = false;
   }
 }
